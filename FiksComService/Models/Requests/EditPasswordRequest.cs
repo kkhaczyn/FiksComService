a5f14e783d3a08ac2675d5031dae7d92ce330267
kkhaczyn <kkhaczyn@gmail.com>
2024-04-06 11:02:19 +0200

    Adding Edit User, Changing Password And List Users

---
+++ FiksComService/Models/Requests/EditPasswordRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace FiksComService.Models.Requests
+{
+    public class EditPasswordRequest
+    {
+        public required int UserId { get; set; }
+        public required string CurrentPassword { get; set; }
+        public required string NewPassword { get; set; }
+
+    }
+}
