16718d85eec256470af9b201bae0fecf78e559bd
kkhaczyn <kkhaczyn@gmail.com>
2024-06-09 12:08:34 +0200

    Tests

---
+++ FiksComServiceTests/Controllers/CartControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using FiksComService.Controllers;
+using FiksComService.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace FiksComServiceTests.Controllers
+{
+    [TestFixture]
+    public class CartControllerTests
+    {
+        private Mock<IComponentRepository> _componentRepositoryMock;
+        private Mock<ISession> _sessionMock;
+        private Mock<HttpContext> _httpContextMock;
+        private CartController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _componentRepositoryMock = new Mock<IComponentRepository>();
+            _sessionMock = new Mock<ISession>();
+            _httpContextMock = new Mock<HttpContext>();
+            _httpContextMock.SetupGet(x => x.Session).Returns(_sessionMock.Object);
+
+            _controller = new CartController(_componentRepositoryMock.Object)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = _httpContextMock.Object
+                }
+            };
+        }
+
+        [Test]
+        public void Buy_ShouldReturnOkWhenItemAdded()
+        {
+            // Arrange
+            _componentRepositoryMock.Setup(x => x.GetComponentById(It.IsAny<int>()))
+                .Returns(new FiksComService.Models.Database.Component()
+                {
+                    ComponentId = 1,
+                    ComponentType = new()
+                    {
+                        Code = "RAM",
+                        Name = "Pamięć RAM"
+                    },
+                    Manufacturer = "Test",
+                    Model = "Test",
+                    Price = 10,
+                    QuantityAvailable = 10
+                });
+
+            // Act
+            var result = _controller.Buy(1);
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual("Dodano do koszyka.", okResult.Value);
+        }
+    }
+}
---
+++ FiksComServiceTests/Controllers/OrderControllerTests.cs
@@ -0,0 +1,109 @@
+﻿using FiksComService.Controllers;
+using FiksComService.Models.Database;
+using FiksComService.Repositories;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Security.Claims;
+
+namespace FiksComServiceTests.Controllers
+{
+    [TestFixture]
+    public class OrderControllerTests
+    {
+        private Mock<IOrderRepository> _orderRepositoryMock;
+        private Mock<IOrderDetailRepository> _orderDetailRepositoryMock;
+        private Mock<IComponentRepository> _componentRepositoryMock;
+        private Mock<IInvoiceRepository> _invoiceRepositoryMock;
+        private Mock<IWebHostEnvironment> _webHostEnvironmentMock;
+        private Mock<UserManager<User>> _userManagerMock;
+        private Mock<HttpContext> _httpContextMock;
+        private OrderController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _orderRepositoryMock = new Mock<IOrderRepository>();
+            _orderDetailRepositoryMock = new Mock<IOrderDetailRepository>();
+            _componentRepositoryMock = new Mock<IComponentRepository>();
+            _invoiceRepositoryMock = new Mock<IInvoiceRepository>();
+            _webHostEnvironmentMock = new Mock<IWebHostEnvironment>();
+
+            var userStoreMock = new Mock<IUserStore<User>>();
+            _userManagerMock = new Mock<UserManager<User>>(userStoreMock.Object, null, null, null, null, null, null, null, null);
+            _httpContextMock = new Mock<HttpContext>();
+
+            _controller = new OrderController(
+                _orderRepositoryMock.Object,
+                _orderDetailRepositoryMock.Object,
+                _componentRepositoryMock.Object,
+                _invoiceRepositoryMock.Object,
+                _webHostEnvironmentMock.Object,
+                _userManagerMock.Object)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = _httpContextMock.Object
+                }
+            };
+        }
+
+        [Test]
+        public async Task GetMyOrders_ShouldReturnUserOrders_WhenUserIsAuthenticated()
+        {
+            // Arrange
+            var user = new User { Id = 1, UserName = "testuser" };
+            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] { new Claim(ClaimTypes.Name, user.UserName) }, "TestAuthType"));
+            _httpContextMock.Setup(x => x.User).Returns(claimsPrincipal);
+
+            _userManagerMock.Setup(x => x.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user);
+            _orderRepositoryMock.Setup(x => x.FindByUserId(user.Id)).Returns(new List<Order>() { new Order() { Status = "Placed"} });
+
+            // Act
+            var result = await _controller.GetMyOrders();
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.IsInstanceOf<IEnumerable<Order>>(okResult.Value);
+        }
+
+        [Test]
+        public async Task GetUserOrders_ShouldReturnUserOrders_WhenUserIsFound()
+        {
+            // Arrange
+            var userId = 1;
+            var user = new User { Id = userId, UserName = "testuser" };
+
+            _userManagerMock.Setup(x => x.FindByIdAsync(userId.ToString())).ReturnsAsync(user);
+            _orderRepositoryMock.Setup(x => x.FindByUserId(userId)).Returns(new List<Order>() { new Order() { Status = "Placed" } });
+
+            // Act
+            var result = await _controller.GetUserOrders(userId);
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.IsInstanceOf<IEnumerable<Order>>(okResult.Value);
+        }
+
+        [Test]
+        public async Task GetUserOrders_ShouldReturnNotFound_WhenUserIsNotFound()
+        {
+            // Arrange
+            var userId = 1;
+
+            _userManagerMock.Setup(x => x.FindByIdAsync(userId.ToString())).ReturnsAsync((User)null);
+
+            // Act
+            var result = await _controller.GetUserOrders(userId);
+
+            // Assert
+            Assert.IsInstanceOf<BadRequestObjectResult>(result);
+        }
+    }
+}
